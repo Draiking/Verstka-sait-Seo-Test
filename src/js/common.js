2f90583e39611b5315b21c84534c04b9a19bc6a1
Gennadya <heavyyrain3039@gmail.com>
2019-09-23 16:45:02 +0300

    content1

--- src/js/common.js
+++ src/js/common.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
 
+    $('.input-name').focus(function () {
+        $(this).addClass('active-input')
+    });
+    $('.input-name').blur(function () {
+        $(this).removeClass('active-input')
+    });
+
+    $('.input-phone').focus(function () {
+        $(this).addClass('active-input')
+    });
+    $('.input-phone').blur(function () {
+        $(this).removeClass('active-input')
+    });
 
 });
